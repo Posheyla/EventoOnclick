864bf6b1aa9dbd4eeb4db3fce76e2a295f3539d3
Posheyla <luisa.virgo.98@hotmail.com>
2022-01-11 00:30:59 -0500

    Tercer ejercicio JS

---
+++ index.js
@@ -0,0 +1,9 @@
+function cambioTexto(elemento){
+    elemento.innerText = "Logout";
+}
+function remover(elemento){
+    elemento.remove();
+}
+function like(elemento){
+    alert(" Ninja was liked");
+}
